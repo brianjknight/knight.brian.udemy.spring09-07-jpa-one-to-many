8886b4de6f889387fa8ad4c36600aab3bf0569c8
Brian Knight <knight.j.brian@gmail.com>
2025-07-10 17:08:19 -0600

    added JOIN FECTH instructorDetail

--- src/main/java/knight/brian/spring/boot/cruddemo/dao/AppDAOImpl.java
+++ src/main/java/knight/brian/spring/boot/cruddemo/dao/AppDAOImpl.java
@@ -80,6 +80,7 @@ public class AppDAOImpl implements AppDAO {
         TypedQuery<Instructor> query = entityManager.createQuery(
                 "select i from Instructor i "
                 + "JOIN FETCH i.courses "
+                + "JOIN FETCH i.instructorDetail " // minimizes additional hibernate query
                 + "where i.id = :data",
                 Instructor.class
         );
